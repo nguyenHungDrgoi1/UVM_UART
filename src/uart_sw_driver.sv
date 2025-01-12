3c2a02fa79361a38659b9717d53e2a129ff903fb
nguyenHungDrgoi1 <hunghihi325@gmail.com>
2025-01-13 00:45:33 +0700

    update driver

--- src/uart_sw_driver.sv
+++ src/uart_sw_driver.sv
@@ -1,6 +1,74 @@
-class uart_sw_driver extends uvm_driver #();
+class uart_sw_driver extends uvm_driver #(uart_sw_item);
      `uvm_component_utils(dti_apb_master_driver)
-     virtual uart_sw_driver;
-
+     virtual uart_sw_intf config_intf;
+     uart_sw_item item;
+     int item_count = 0;
      
-endclass
+     extern function new(sting name = "uart_sw_driver", uvm_component parent = null);
+     extern task run_phase(uvm_phase phase);
+     extern function void build_phase(uvm_phase phase);
+
+     extern task drive();
+     extern task check_reset();
+     
+endclass: uart_sw_driver
+
+function uart_sw_driver::new(sting name = "uart_sw_driver", uvm_component parent = null);
+     super.new(name,parent);
+endfunction
+
+task uart_sw_driver::run_phase(uvm_phase phase);
+
+     @(negedge config_intf.reset_n);
+          config_intf.tx_data <= 0;
+          //config signal for reset
+     @(posedge config_intf.reset_n)
+     @(posedge config_intf.clk)
+     item =new;
+     fork
+          check_reset();
+     join_none
+
+     forever begin
+          while(~config_intf.reset_n)
+               @(posedge config_intf.clk);
+               seq_item_port.get_next_item(item);
+
+          fork
+               drive();
+               check_reset();
+          join_any
+          disable fork;
+
+          // without rsp
+          //if(item)
+
+          seq_item_port.item_done();
+     end
+endtask : run_phase
+
+task uart_sw_driver::check_reset();
+     wait(~config_intf.reset_n)
+     //config for signal in reset
+     @(posedge config.clk);
+endtask
+
+task uart_sw_driver::drive();
+int current_idx = 0;
+'uvm_infor("config_uart",$sformatf("hello chua biet fill gi"),UVM_MEDIUM)
+repeat (item.burst_len) begin
+     //idle
+     while(config_intf.reset = 1'b0) begin
+      @(posedge config_intf)
+     end
+     // send_data
+     config_intf.tx_data <= item. tx_data;
+     config_intf.rx_data <= item.rx_data;
+     //gan tien hieu can tuyen
+end 
+endtask
+
+function void uart_sw_driver::build_phase(uvm_phase phase)
+
+//chưa biet fill gi :)))
+endfunction: build_phase
--- src/uart_sw_item.sv
+++ src/uart_sw_item.sv
@@ -1,11 +1,10 @@
 class uart_sw_item extends uvm_sequence_item;
-    rand [7:0] rx_data[];
-    rand [7:0] tx_data[];
+    rand [7:0] rx_data;
+    rand [7:0] tx_data;
+    rand int burst_len;
     `uvm_object_utils_begin(uart_sw_item)
-    `uvm_field_array_int ( apb_data,   UVM_ALL_ON)
-    `uvm_field_array_int ( apb_addr,    UVM_ALL_ON)
-    `uvm_field_array_int ( apb_write,   UVM_ALL_ON)
-    `uvm_field_array_int ( apb_strb,    UVM_ALL_ON)
+    `uvm_field_array_int ( rx_data,   UVM_ALL_ON)
+    `uvm_field_array_int ( tx_data,    UVM_ALL_ON)
     `uvm_field_int ( burst_len,   UVM_ALL_ON)
 `uvm_object_utils_end
     extern function new(string name = "uart_sw_item");
